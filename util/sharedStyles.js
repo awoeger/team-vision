564dab1384b34da73f58333566f82313e4529826
Andrea Wöger <andrea.woeger@gmx.net>
2021-06-21 17:08:27 +0200

    Add sharedStyles.js

---
+++ util/sharedStyles.js
@@ -0,0 +1,9 @@
+// Color Palette
+export const darkBlue = '#1d2a48';
+export const lightBlue = '#41d4dc';
+export const beige = '#f8dad0';
+
+// Text sizes
+export const normalText = '16px';
+export const smallText = '0.75rem';
+export const largeText = '1.5rem';
