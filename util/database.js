cc7fbcd02c96e2b3e03664a4c0d4022e65b0d44d
Andrea Wöger <andrea.woeger@gmx.net>
2021-06-22 15:50:45 +0200

    Create .env and .env example

--- util/database.js
+++ util/database.js
@@ -2,5 +2,7 @@ import camelcaseKeys from 'camelcase-keys';
 import dotenvSafe from 'dotenv-safe';
 import postgres from 'postgres';
 
+dotenvSafe.config();
+
 // Connect to PostgreSQL
 const sql = connectOneTimeToDatabase();
