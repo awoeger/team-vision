6650c8a21e4a8bdbe9986f2ecf36f89b79ea1bc2
Andrea Wöger <andrea.woeger@gmx.net>
2021-06-25 18:04:18 +0200

    Delete requests table

--- migrations/1624373253-create-team_user-table.js
+++ migrations/1624373253-create-team_user-table.js
@@ -3,6 +3,7 @@ exports.up = async function up(sql) {
 		CREATE TABLE team_user (
 			users_id INT REFERENCES users(id) NOT NULL,
 			team_id INT REFERENCES teams(id) NOT NULL,
+			status_id INT REFERENCES status_types(id) NOT NULL,
 			position_on_team varchar(50),
 			playing_since DATE
 		)
