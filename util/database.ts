30b18ba0207383bd6ff0bcbabae7880e0ab8ecf5
Andrea Wöger <andrea.woeger@gmx.net>
2021-07-19 15:56:23 +0200

    Add ley.config.js

--- util/database.ts
+++ util/database.ts
@@ -10,7 +10,7 @@ import {
   User,
   UserWithPasswordHash,
 } from '../util/types';
-import setPostgresDefaultsOnHeroku from './setPostgresDefaultsOnHeroku';
+import setPostgresDefaultsOnHeroku from './setPostgresDefaultsOnHeroku.js';
 
 setPostgresDefaultsOnHeroku();
 
