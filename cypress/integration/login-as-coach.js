4e52fe3aa38a3abf2eb35c490d03eac8dc9ecfd3
Andrea Wöger <andrea.woeger@gmx.net>
2021-07-19 14:37:53 +0200

    Comment out cypress tests

--- cypress/integration/login-as-coach.js
+++ cypress/integration/login-as-coach.js
@@ -1,35 +1,35 @@
-describe('Can login as coach', () => {
-  it('Can login as coach, create new team, go to team, can create event, can access all team pages', () => {
-    cy.visit('http://localhost:3000/');
-    // Login
-    cy.get('[data-cy="header-login-link"]').click();
-    cy.get('[data-cy="login-username"]').type('user_coach_test');
-    cy.get('[data-cy="login-password"]').type('triA1-PA55w0rd1');
-    cy.contains('LOGIN').click();
-    // Create new team
-    cy.get('[data-cy="create-new-team"]').click();
-    cy.get('[data-cy="create-new-team-teamname"]').type('Test Team');
-    cy.get('[data-cy="create-new-team-sporttype"]').type('Test Sport');
-    cy.get('[data-cy="create-new-team-date"]').type('xx/yyyy');
-    cy.contains('CREATE NEW TEAM').click();
-    cy.contains('GO TO TEAM').click();
-    cy.contains('CREATE EVENT').click();
-    cy.get('[data-cy=""]').type('Test Team');
-    cy.get('[data-cy="create-new-team-sporttype"]').type('Test Sport');
-    cy.get('[data-cy="create-new-team-date"]').type('xx/yyyy');
-    cy.contains('GO TO TEAM').click();
-    // Create Event
-    cy.contains('CREATE EVENT').click();
-    cy.get('[data-cy="event-eventtype"]').select('Training');
-    cy.get('[data-cy="event-start-date"]').type('01.01.1111');
-    cy.get('[data-cy="event-meeting-time"]').type('11:11');
-    cy.get('[data-cy="event-start-time"]').type('12:12');
-    cy.get('[data-cy="event-end-time"]').type('13:13');
-    cy.get('[data-cy="event-location"]').type('Test Location');
-    cy.get('[data-cy="event-description"]').type('Test Test Test');
-    cy.contains('CREATE EVENT').click();
-    cy.contains('SEE EVENT DETAILS').click();
-    cy.contains('EXERCISES').click();
-    cy.contains('TEAM MEMBERS').click();
-  });
-});
+// describe('Can login as coach', () => {
+//   it('Can login as coach, create new team, go to team, can create event, can access all team pages', () => {
+//     cy.visit('http://localhost:3000/');
+//     // Login
+//     cy.get('[data-cy="header-login-link"]').click();
+//     cy.get('[data-cy="login-username"]').type('user_coach_test');
+//     cy.get('[data-cy="login-password"]').type('triA1-PA55w0rd1');
+//     cy.contains('LOGIN').click();
+//     // Create new team
+//     cy.get('[data-cy="create-new-team"]').click();
+//     cy.get('[data-cy="create-new-team-teamname"]').type('Test Team');
+//     cy.get('[data-cy="create-new-team-sporttype"]').type('Test Sport');
+//     cy.get('[data-cy="create-new-team-date"]').type('xx/yyyy');
+//     cy.contains('CREATE NEW TEAM').click();
+//     cy.contains('GO TO TEAM').click();
+//     cy.contains('CREATE EVENT').click();
+//     cy.get('[data-cy=""]').type('Test Team');
+//     cy.get('[data-cy="create-new-team-sporttype"]').type('Test Sport');
+//     cy.get('[data-cy="create-new-team-date"]').type('xx/yyyy');
+//     cy.contains('GO TO TEAM').click();
+//     // Create Event
+//     cy.contains('CREATE EVENT').click();
+//     cy.get('[data-cy="event-eventtype"]').select('Training');
+//     cy.get('[data-cy="event-start-date"]').type('01.01.1111');
+//     cy.get('[data-cy="event-meeting-time"]').type('11:11');
+//     cy.get('[data-cy="event-start-time"]').type('12:12');
+//     cy.get('[data-cy="event-end-time"]').type('13:13');
+//     cy.get('[data-cy="event-location"]').type('Test Location');
+//     cy.get('[data-cy="event-description"]').type('Test Test Test');
+//     cy.contains('CREATE EVENT').click();
+//     cy.contains('SEE EVENT DETAILS').click();
+//     cy.contains('EXERCISES').click();
+//     cy.contains('TEAM MEMBERS').click();
+//   });
+// });
--- cypress/integration/register-as-coach.js
+++ cypress/integration/register-as-coach.js
@@ -1,18 +1,18 @@
-describe('Can register as coach', () => {
-  it('Can click through landing page, register as coach', () => {
-    cy.visit('http://localhost:3000/');
-    cy.contains('About');
-    cy.get('[data-cy="header-about-link"]').click();
-    cy.get('[data-cy="header-guide-link"]').click();
-    cy.get('[data-cy="header-contact-link"]').click();
-    cy.get('[data-cy="header-login-link"]').click();
-    cy.contains('Register now').click();
-    cy.get('[data-cy="registration-first-name"]').type('User Coach');
-    cy.get('[data-cy="registration-last-name"]').type('Test');
-    cy.get('[data-cy="registration-username"]').type('user_coach_test');
-    cy.get('[data-cy="registration-email"]').type('user_coach_test@gmail.com');
-    cy.get('[data-cy="registration-password"]').type('triA1-PA55w0rd1');
-    cy.get('[data-cy="registration-role-coach"]').select('Coach');
-    cy.contains('CREATE ACCOUNT').click();
-  });
-});
+// describe('Can register as coach', () => {
+//   it('Can click through landing page, register as coach', () => {
+//     cy.visit('http://localhost:3000/');
+//     cy.contains('About');
+//     cy.get('[data-cy="header-about-link"]').click();
+//     cy.get('[data-cy="header-guide-link"]').click();
+//     cy.get('[data-cy="header-contact-link"]').click();
+//     cy.get('[data-cy="header-login-link"]').click();
+//     cy.contains('Register now').click();
+//     cy.get('[data-cy="registration-first-name"]').type('User Coach');
+//     cy.get('[data-cy="registration-last-name"]').type('Test');
+//     cy.get('[data-cy="registration-username"]').type('user_coach_test');
+//     cy.get('[data-cy="registration-email"]').type('user_coach_test@gmail.com');
+//     cy.get('[data-cy="registration-password"]').type('triA1-PA55w0rd1');
+//     cy.get('[data-cy="registration-role-coach"]').select('Coach');
+//     cy.contains('CREATE ACCOUNT').click();
+//   });
+// });
