d413e1feb17f5946931a744c5a87ff919ac1f5ce
Andrea Wöger <andrea.woeger@gmx.net>
2021-07-20 11:02:32 +0200

    Delete manifest.json link

--- cypress/integration/register-as-player.js
+++ cypress/integration/register-as-player.js
@@ -1,14 +1,14 @@
-// describe('Can register as coach', () => {
-//   it('Can click through landing page, register as coach', () => {
-//     cy.visit('http://localhost:3000/');
-//     cy.get('[data-cy="header-login-link"]').click();
-//     cy.contains('Register now').click();
-//     cy.get('[data-cy="registration-first-name"]').type('User Player');
-//     cy.get('[data-cy="registration-last-name"]').type('Test');
-//     cy.get('[data-cy="registration-username"]').type('user_player_test');
-//     cy.get('[data-cy="registration-email"]').type('user_player_test@gmail.com');
-//     cy.get('[data-cy="registration-password"]').type('triA1-PA55w0rd2');
-//     cy.get('[data-cy="registration-role-coach"]').select('Player');
-//     cy.contains('CREATE ACCOUNT').click();
-//   });
-// });
+describe('Can register as coach', () => {
+  it('Can click through landing page, register as coach', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('[data-cy="header-login-link"]').click();
+    cy.contains('Register now').click();
+    cy.get('[data-cy="registration-first-name"]').type('User Player');
+    cy.get('[data-cy="registration-last-name"]').type('Test');
+    cy.get('[data-cy="registration-username"]').type('user_player_test');
+    cy.get('[data-cy="registration-email"]').type('user_player_test@gmail.com');
+    cy.get('[data-cy="registration-password"]').type('triA1-PA55w0rd2');
+    cy.get('[data-cy="registration-role-coach"]').select('Player');
+    cy.contains('CREATE ACCOUNT').click();
+  });
+});
